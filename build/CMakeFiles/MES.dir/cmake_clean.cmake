cc3b50b2a8be63e606d9b3083b77fb10386d6f68
Bernardo Gabriel <bernardoacgabriel@gmail.com>
2023-02-28 22:49:57 +0000

    Initial commit

---
+++ build/CMakeFiles/MES.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MES.dir/C_/Users/berna/Documents/INFI/MES/main.cpp.obj"
+  "CMakeFiles/MES.dir/C_/Users/berna/Documents/INFI/MES/main.cpp.obj.d"
+  "MES.exe"
+  "MES.exe.manifest"
+  "MES.pdb"
+  "libMES.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int i = 0;
+    while(1)
+    {
+        i++;
+        std::cout << i << std::endl;
+    }
+}
