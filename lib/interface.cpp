202481548674226d4c4a8d8937202bb6b8956e3d
elcapitanu <bernardoacgabriel@gmail.com>
2023-05-17 11:59:23 +0100

    Interface: added interface to check factory status

--- lib/interface.cpp
+++ lib/interface.cpp
@@ -59,7 +59,7 @@ void GUI::show()
              << endl
              << "a. MES1" << endl
              << "b. MES2" << endl
-             << "c. MES3" << endl
+             << "c. Show factory" << endl
              << endl
              << "x. Close" << endl
              << endl
@@ -111,20 +111,23 @@ void GUI::show()
     }
     else if (display.state == 3)
     {
-        cout << "\033[7;36m*********************MES3**********************\033[0m" << endl
+        cout << "\033[7;36m*******************Factory*********************\033[0m" << endl
              << endl
              << name << "      " << ctime(&date) << endl
-             << endl;
-
-        if (messi->day > 0)
-        {
-            cout << "Day: " << messi->day << endl
-                 << endl
-                 << "Number of orders: " << messi->orders << endl
-                 << endl;
-        }
-        cout << "\033[7;36m***********************************************\033[0m" << endl
-             << "Server: " << soc->message << endl
-             << "\033[7;36m************Ainda não faço nada :)*************\033[0m" << endl;
+             << endl
+             << "          Space in the warehouse: " << messi->fac.war_space << endl
+             << endl
+             << "  | P1: " << messi->fac.p[0] << " | P2: " << messi->fac.p[1] << " | P3: " << messi->fac.p[2] << " | P4: " << messi->fac.p[3] << " | P5: " << messi->fac.p[4] << " |" << endl
+             << "      | P6: " << messi->fac.p[5] << " | P7: " << messi->fac.p[6] << " | P8: " << messi->fac.p[7] << " | P9: " << messi->fac.p[8] << " |" << endl
+             << endl
+             << "    | M1: T" << messi->fac.m[0] << " | M2: T" << messi->fac.m[1] << " | M3: T" << messi->fac.m[2] << " | M4: T" << messi->fac.m[3] << " |" << endl
+             << endl
+             << "\033[7;36m***********************************************\033[0m" << endl
+             << endl
+             << "q. Return to MENU" << endl
+             << endl
+             << "x. Close" << endl
+             << endl
+             << "\033[7;36m***********************************************\033[0m" << endl;
     }
 }
